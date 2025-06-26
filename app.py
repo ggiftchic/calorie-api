e34c420e86a77c61a9f159fe83be44f84b4db3d2
ggiftchic <wasutida.l@lmwn.com>
2025-06-26 16:13:41 +0700

    Fix: set dynamic port for Render

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def analyze():
         }), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=5001)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
